40c2f6132b203c15af088b4a0c95a7a02a6a625a
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2023-02-15 18:07:47 +0200

    refactor navbar component and create home component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     return (
         <>
             <Navbar/>
-            {/*<Home />*/}
+            <Home />
             {/*<Main />*/}
             {/*<Footer />*/}
         </>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import './navbar.css';
+import './navbar.scss';
 import {MdOutlineTravelExplore} from 'react-icons/md';
 import {AiFillCloseCircle} from 'react-icons/ai';
 import {TbGridDots} from "react-icons/tb";
 
 const Navbar = () => {
-    const [active, setActive] = useState('navbar');
+    const [active, setActive] = useState('navBar');
     const showNav = () => {
         setActive('navBar activeNavbar');
     }
