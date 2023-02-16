20e231ff8d7fa8b4522e85e7aaf922a9860f9340
MihaelaNaidin040696 <naidinmihaela76@gmail.com>
2023-02-16 13:20:26 +0200

    made data list of objects global

---
+++ src/data.js
@@ -0,0 +1,57 @@
+import img from "./Assets/img.jpg";
+import img2 from "./Assets/img2.jpg";
+import img3 from "./Assets/img3.jpg";
+import img4 from "./Assets/img4.jpg";
+import img5 from "./Assets/img5.jpg";
+
+export const data = [
+  {
+    id: 1,
+    imgSrc: img,
+    destTitle: 'Bora Bora',
+    location: 'New Zealand',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the world. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id: 2,
+    imgSrc: img2,
+    destTitle: 'Machu Picchu',
+    location: 'Peru',
+    grade: 'CULTURAL RELAX',
+    fees: '$600',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the Sun Gate is simply spectacular.'
+  },
+
+  {
+    id: 3,
+    imgSrc: img3,
+    destTitle: 'Bali Island',
+    location: 'Indonesia',
+    grade: 'CULTURAL RELAX',
+    fees: '$500',
+    description: 'Bali is an Indonesian Island and one of the best holiday destinations in the Indonesian archipelago. Bali is known for its volcanic mountains, history, art & culture, food, temples and beautiful sandy beaches.'
+  },
+
+  {
+    id: 4,
+    imgSrc: img4,
+    destTitle: 'Angkor Wat',
+    location: 'Cambodia',
+    grade: 'CULTURAL RELAX',
+    fees: '$790',
+    description: 'Angkot wat represents the entire range of Khmer art from the 9th to the 15th century. This temple is considered the heart and soul of Cambodia. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id: 5,
+    imgSrc: img5,
+    destTitle: 'Cinque Terre',
+    location: 'Italy',
+    grade: 'CULTURAL RELAX',
+    fees: '$840',
+    description: 'The vibrant hues of the houses are its benchmark and explain the beauty of this place. Also, if you are a foodie and love seafood, you will be exhilarated with the choice you are about to get here. Happy exploring great food!'
+  }
+];
